f8c396c111287b2e9353a988cc1489588119e5b9
Muhammad Iqbal Septian <yakuzuyoa@gmail.com>
2023-06-22 20:44:19 +0700

    edit project

---
+++ components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      
+    </header>
+  )
+}
--- pages/blog.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Blog() {
-  return (
-    <p className='text-3xl underline font-bold'>Ini Halaman Blog!</p>
-  )
-}
