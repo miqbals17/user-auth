7329a12f1764988461f669c7559c587a49d56c83
Muhammad Iqbal Septian <yakuzuyoa@gmail.com>
2023-06-23 10:09:04 +0700

    fix login button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import Style from './header.module.css';
 
 const navigation = [
   { name: 'Beranda', href: '/', current: true },
-  { name: 'Profil', href: '#', current: false },
+  { name: 'Profil', href: '/profil', current: false },
   { name: 'Kontak', href: '#', current: false },
 ]
 
@@ -16,12 +15,15 @@ export default function Header() {
       </div>
       <nav className='flex space-x-4'>
         {navigation.map((item) => (
-          <Link key={item.name} href={item.href} className='text-white px-3 py-2 rounded-md font-medium hover:bg-white hover:text-gray-800'>{item.name}</Link>
+          <Link key={item.name} href={item.href} className={`px-3 py-2 rounded-md font-medium ${item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}>{item.name}</Link>
         ))}
       </nav>
-      <div className='transition duration-500 hover:translate-x-1 hover:bg-gray-900 rounded-lg p-2'>
+      {/* <div className='transition duration-500 hover:translate-x-1 hover:bg-gray-900 rounded-lg p-2'>
         <Link href='/login' className='font-bold text-white' target='_blank'>Login <span>&#8594;</span></Link>
-      </div>
+      </div> */}
+      <Link href='/login' className='group font-bold text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
+        Login<span className='inline-block group-hover:translate-x-1 duration-500 ml-2'>&#8594;</span>
+      </Link>
     </header>
   )
 }
---
+++ pages/profil.tsx
@@ -0,0 +1,12 @@
+import Metadata from '@/components/Head/metadata'
+import Header from '@/components/Header/Header'
+import React from 'react'
+
+export default function Profil() {
+  return (
+    <>
+      <Metadata title='Profil' />
+      <Header />
+    </>
+  )
+}
