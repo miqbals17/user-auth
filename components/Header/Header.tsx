687f478df1223c10550a41c42b9191e299e31425
Muhammad Iqbal Septian <yakuzuyoa@gmail.com>
2023-06-23 10:10:25 +0700

    delete old login

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -18,9 +18,6 @@ export default function Header() {
           <Link key={item.name} href={item.href} className={`px-3 py-2 rounded-md font-medium ${item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}>{item.name}</Link>
         ))}
       </nav>
-      {/* <div className='transition duration-500 hover:translate-x-1 hover:bg-gray-900 rounded-lg p-2'>
-        <Link href='/login' className='font-bold text-white' target='_blank'>Login <span>&#8594;</span></Link>
-      </div> */}
       <Link href='/login' className='group font-bold text-white hover:bg-gray-700 px-4 py-2 rounded-full'>
         Login<span className='inline-block group-hover:translate-x-1 duration-500 ml-2'>&#8594;</span>
       </Link>
