12256c0ae953cf60f10ca31becaf8fcfb254cc80
Muhammad Iqbal Septian <yakuzuyoa@gmail.com>
2023-06-23 21:55:50 +0700

    navbar done

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -40,6 +40,22 @@ export default function Header(props: Props) {
                 </Disclosure.Button>
               </div>
             </div>
+
+            <Disclosure.Panel className='sm:hidden'>
+              <div className='space-y-1 px-2 pb-3 pt-2'>
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className={`block rounded-md px-3 py-2 text-base font-medium ${item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}
+                  >
+                      {item.name}
+                  </Disclosure.Button>
+                ))}
+                <Disclosure.Button as='a' href='/login' className='block px-3 py-2 text-base font-medium bg-white text-gray-900 rounded-md'>Masuk</Disclosure.Button>
+              </div>
+            </Disclosure.Panel>
           </>
         )}
       </Disclosure>
