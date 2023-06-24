81bc4d5d261e97dde9559442e5aabbc9754c1450
Muhammad Iqbal Septian <yakuzuyoa@gmail.com>
2023-06-24 13:55:31 +0700

    form submit done

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,38 @@
 import Header from "@/components/Header/Header";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+interface Data {
+  email: string,
+  password: string
+}
+
+async function registerData(data: Data) {
+  const res = await fetch('http://localhost:5000/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
 
 export default function Register() {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    e.preventDefault();
+    console.log(formData);
+    router.push('/')
+  }
+
   return (
     <>
       <Header />
@@ -13,28 +43,29 @@ export default function Register() {
         </div>
 
         <div className="mt-10 sm:max-w-sm sm:mx-auto sm:w-full">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
             <div>
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
               <div className="mt-2">
-                <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 ring-1 py-1.5 text-gray-900 placeholder:text-gray-400" />
+                <input id="email" name="email" type="email" autoComplete="email" required className="outline-0 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
               </div>
             </div>
 
             <div>
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
               <div className="mt-2">
-                <input id="password" name="password" type="password" autoComplete="password" required className="block w-full rounded-md border-0 ring-1 py-1.5 text-gray-900" />
+                <input id="password" name="password" type="password" autoComplete="password" required className="outline-0 block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
               </div>
             </div>
 
             <div>
-              <button type="submit" className="w-full rounded-md bg-indigo-600 px-3 py-1.5 font-semibold text-white shadow-sm hover:bg-indigo-500">Login</button>
-              <div className="mt-2">
-                <p>Sudah Memiliki Akun? <Link href='/login' className="text-indigo-600">Login</Link></p>
-              </div>
+              <button type="submit" className="flex justify-center w-full rounded-md bg-indigo-600 px-3 py-1.5 font-semibold text-white shadow-sm hover:bg-indigo-500">Login</button>
             </div>
           </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Sudah Memiliki Akun? <Link href='/login' className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login</Link>
+          </p>
         </div>
       </div>
     </>
